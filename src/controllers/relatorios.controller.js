e42ecef4352921c5b43e16b65babe2ffc1445db7
Gugzz21 <gustavodinizz966@gmail.com>
2026-05-13 11:57:17 -0300

    asdsadsa

--- src/controllers/relatorios.controller.js
+++ src/controllers/relatorios.controller.js
@@ -1,202 +1,222 @@
 const service = require('../services/relatorio.service');
 
-async function gerarSnapshotLivro(req, res) {
+async function gerarSnapshotLivro(req, res, next) {
     try {
         const data = await service.gerarSnapshotLivro(req.body);
         res.send(201, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function dashboardKpis(req, res) {
+async function dashboardKpis(req, res, next) {
     try {
         const data = await service.dashboardKpis();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function topLivrosLidos(req, res) {
+async function topLivrosLidos(req, res, next) {
     try {
         const data = await service.topLivrosLidos();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function usuariosInadimplentes(req, res) {
+async function usuariosInadimplentes(req, res, next) {
     try {
         const data = await service.usuariosInadimplentes();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function criar(req, res) {
+async function criar(req, res, next) {
     try {
         const data = await service.criar(req.body);
         res.send(201, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function listar(req, res) {
+async function listar(req, res, next) {
     try {
         const data = await service.listar();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function buscarPorId(req, res) {
+async function buscarPorId(req, res, next) {
     try {
         const data = await service.buscarPorId(req.params.id);
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function deletar(req, res) {
+async function deletar(req, res, next) {
     try {
         await service.deletar(req.params.id);
         res.send(204);
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function alterarStatus(req, res) {
+async function alterarStatus(req, res, next) {
     try {
         const data = await service.alterarStatus(req.params.id, req.body);
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function limparAntigos(req, res) {
+async function limparAntigos(req, res, next) {
     try {
         const data = await service.limparAntigos();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function exportarCSV(req, res) {
+async function exportarCSV(req, res, next) {
     try {
         const data = await service.exportarCSV();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function listarSnapshotLivros(req, res) {
+async function listarSnapshotLivros(req, res, next) {
     try {
         const data = await service.listarSnapshotLivros();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function buscarSnapshotLivroId(req, res) {
+async function buscarSnapshotLivroId(req, res, next) {
     try {
         const data = await service.buscarSnapshotLivroId(req.params.id);
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function gerarSnapshotUsuario(req, res) {
+async function gerarSnapshotUsuario(req, res, next) {
     try {
         const data = await service.gerarSnapshotUsuario(req.body);
         res.send(201, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function listarSnapshotUsuarios(req, res) {
+async function listarSnapshotUsuarios(req, res, next) {
     try {
         const data = await service.listarSnapshotUsuarios();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function buscarSnapshotUsuarioId(req, res) {
+async function buscarSnapshotUsuarioId(req, res, next) {
     try {
         const data = await service.buscarSnapshotUsuarioId(req.params.id);
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function gerarSnapshotEmprestimo(req, res) {
+async function gerarSnapshotEmprestimo(req, res, next) {
     try {
         const data = await service.gerarSnapshotEmprestimo(req.body);
         res.send(201, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function listarSnapshotEmprestimos(req, res) {
+async function listarSnapshotEmprestimos(req, res, next) {
     try {
         const data = await service.listarSnapshotEmprestimos();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function buscarSnapshotEmprestimoId(req, res) {
+async function buscarSnapshotEmprestimoId(req, res, next) {
     try {
         const data = await service.buscarSnapshotEmprestimoId(req.params.id);
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
-async function totalMultas(req, res) {
+async function totalMultas(req, res, next) {
     try {
         const data = await service.totalMultas();
         res.send(200, { success: true, data });
+        return next();
         
     } catch (err) {
-        throw err;
+        return next(err);
     }
 }
 
